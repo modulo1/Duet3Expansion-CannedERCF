72b64d2ee70ee423caa92d96ca5fabfa9b71d64c
omtek <30335181+modulo1@users.noreply.github.com>
2025-07-22 19:59:28 -0400

    Update Version.h

--- src/Version.h
+++ src/Version.h
@@ -8,7 +8,7 @@
 #ifndef SRC_VERSION_H_
 #define SRC_VERSION_H_
 
-#define VERSION		"3.6.0+3"
+#define VERSION		"3.6.0+001"
 
 extern const char *const DateText;
 extern const char *const TimeSuffix;
